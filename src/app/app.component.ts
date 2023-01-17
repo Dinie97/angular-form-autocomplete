058ef7ee8f673597679f996fa91c409d42d9aec7
diniesyahirah <dinie.syahirah@gofreightz.com>
2023-01-17 11:55:49 +0800

    Angular-Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {GooglePlaceDirective} from "ngx-google-places-autocomplete";
+import {GeocoderAddressComponent} from "@agm/core";
+import {Options} from "ngx-google-places-autocomplete/objects/options/options";
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { Address } from 'ngx-google-places-autocomplete/objects/address';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-map';
+
+  options: Options = new Options({
+    componentRestrictions: {country: 'MY'}
+  });
+  lat = 28.704060;
+  long = 77.102493;
+  googleMapType = 'satellite';
+  form: FormGroup;
+  @ViewChild("placesRef") placesRef : GooglePlaceDirective;
+  constructor( private fb: FormBuilder,private _snackBar: MatSnackBar){
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNum: ['', Validators.required],
+      address: ['', Validators.required],
+      postcode: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+    })
+  }
+  public handleAddressChange(address: Address) {
+    // Do some stuff
+    var address1 = '';
+    var _route = '';
+    var postalCode = '';
+    var postalCodeSuffix = '';
+    var locality = '';
+    var state = '';
+    var _country = '';
+    var placeName = '';
+    var portName = '';
+   
+    for (const component of address.address_components as GeocoderAddressComponent[]) {
+      const type = component.types[0];
+      console.log(component)
+      switch (type) {
+        case "street_number": {
+            address1 = `${component.long_name}`;
+            break;
+        }
+        case "route": {
+            address1 += `${component.short_name}`;
+            break;
+        }
+        case "postal_code": {
+            postalCode = `${component.long_name}`;
+            break;
+        }
+        case "postal_code_suffix": {
+            postalCode = `${component.long_name}`;
+            break;
+        }
+        case "locality": {
+            locality = `${component.long_name}`;
+            break
+        }
+        case "administrative_area_level_1": {
+            state = `${component.short_name}`;
+            break;
+        }
+        case "country": {
+            _country = `${component.long_name}`;
+            break;
+        }
+    }
+      
+    }
+
+    this.form.patchValue({
+      address: address.name,
+      state: state,
+      country: _country,
+      postcode : postalCode,
+      city: locality
+
+  })
+    
+}
+
+resetForm( x:any ) {
+  if (x.target.value == '')
+      this.form.patchValue({
+        address: '',
+        state: '',
+        country: '',
+        postcode : '',
+        city: ''
+
+      })
+}
+
+submit() {
+  this._snackBar.open('Account Created', 'success');
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core';
+import { environment } from 'src/environments/environment';
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {
+  MatIconModule
+} from '@angular/material/icon';
+import {
+  MatFormFieldModule
+} from '@angular/material/form-field';
+import {
+  MatInputModule
+} from '@angular/material/input';
+import {
+  MatSelectModule
+} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FormsModule,
+    GooglePlaceModule,  
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.MAP_API
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
